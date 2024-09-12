ecb0d0e04c4f28947fd166374cbda30dd7a74145
manhdeptraii <155596926+manhdeptraii@users.noreply.github.com>
2024-09-12 16:18:14 +0700

    Add project files.

---
+++ Lab12_PH52988/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab12_PH52988.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
---
+++ Lab12_PH52988/Controllers/StudentController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab12_PH52988.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();//trả về view tương ứng với tên của action
+        }
+        public IActionResult Register()
+        {
+            return View();//trả về view tương ứng với tên của action
+        }
+        public IActionResult GoHome()
+        {
+            //return RedirectToAction("Index","Home");// điều hướng tới action mà bạn cần
+           
+            return Redirect("/Home/index");// trỏ trực tiếp tới view mà bạn cần, nếu không có logic nào chả
+        }
+    }
+}
